e226d24303d9e8bfafaeeb2984c4ec7c76f6ae7f
616620131 <616620131@qq.com>
2020-01-02 01:11:22 +0800

    首次推送

---
+++ js/sou.js
@@ -0,0 +1,20 @@
+/*
+作者:D.Young
+主页：https://yyv.me/
+github：https://github.com/5iux/sou
+日期：2019-07-26
+版权所有，请勿删除
+*/
+$(document).ready(function () {
+    //菜单点击
+    $("#menu").click(function (event) {
+        $(this).toggleClass('on');
+        $(".list").toggleClass('closed');
+        $(".mywth").toggleClass('hidden');
+    });
+    $("#content").click(function (event) {
+        $(".on").removeClass('on');
+        $(".list").addClass('closed');
+        $(".mywth").removeClass('hidden');
+    });
+});
