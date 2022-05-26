3fcdc80ae03d4345ad380336d12716a3b429e87e
zhangdi <45026982+zzd@users.noreply.github.com>
2022-05-26 18:12:44 +0800

    update package

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,7 @@
     <router-view></router-view>
     <!--    <Background @background="set_background"></Background>-->
     <div id="message"></div>
-    <div id="foot">©2018-2021
+    <div id="foot">©2018-2022
       <a class="out_link" href="https://zhangdi.net/" target="_blank">ZHANGDI.</a> All rights reserved.
       <a class="out_link beian" href="http://beian.miit.gov.cn/" target="_blank">蜀ICP备18024871号</a>
 <!--      <a href="https://github.com/zzd/Simple-Search-Page" style="font-size: 12px;" target="_blank">-->
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@
  * Copyright (c) 2021. ZHANGDI Studio All Rights Reserved.
  */
 module.exports={
-    publicPath:'/Simple-Search-Page',
+    publicPath:'/',
 };
