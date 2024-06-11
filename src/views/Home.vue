05643e8441aad67b595e995d4e2e826f23e14dc5
zhangdi <45026982+zzd@users.noreply.github.com>
2024-06-11 18:54:24 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- <div id="logo"><a href="https://hao.zdsr.cn"><img alt="简洁导航" src="../assets/img/simple-so.svg"> </a></div> -->
-  <div id="logo"><a href="/"><img alt="简洁导航" src="./img/simple-so.svg"> </a></div>
+  <div id="logo"><a href="/"><img alt="简洁导航" src="../assets/img/simple-so.svg"> </a></div>
 
   <div v-if="engines" id="site-main">
     <div id="search-bar">
